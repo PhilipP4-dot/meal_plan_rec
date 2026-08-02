486432aacbf8b40d1a0e4a030aa63a3005151496
Philip Pearce-Pearson <122912060+PhilipP4-dot@users.noreply.github.com>
2026-08-02 17:44:35 -0400

    Bind local server for container deployment

--- web/app.py
+++ web/app.py
@@ -209,4 +209,4 @@ if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "yes")
     port = int(os.getenv("FLASK_PORT", "5000"))
     
-    app.run(debug=debug_mode, port=port, host="127.0.0.1")
+    app.run(debug=debug_mode, port=port, host="0.0.0.0")
